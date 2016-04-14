c83cbb657d92fe54de0bc008d7c917f9776c23c9
RamWire <renrun@zhaofuyang.local>
2016-04-14 15:34:04 +0800

    Add Carthage support.
    Signed-off-by: RamWire <renrun@zhaofuyang.local>

---
+++ NinaPagerViewCarthage/NinaPagerViewCarthage.h
@@ -0,0 +1,21 @@
+//
+//  NinaPagerViewCarthage.h
+//  NinaPagerViewCarthage
+//
+//  Created by RamWire on 16/4/14.
+//  Copyright © 2016年 赵富阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NinaPagerView.h"
+#import "UIParameter.h"
+
+//! Project version number for NinaPagerViewCarthage.
+FOUNDATION_EXPORT double NinaPagerViewCarthageVersionNumber;
+
+//! Project version string for NinaPagerViewCarthage.
+FOUNDATION_EXPORT const unsigned char NinaPagerViewCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NinaPagerViewCarthage/PublicHeader.h>
+
+
